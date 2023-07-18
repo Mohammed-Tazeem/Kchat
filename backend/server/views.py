b6fb769b4f5cc8083904279e0a54df0fb129d805
Mohammed <mtkhanalig@gmail.com>
2023-07-18 20:18:30 +0530

    starting Again

--- backend/server/views.py
+++ backend/server/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.exceptions import AuthenticationFailed, ValidationError
@@ -8,39 +9,40 @@ from .serializer import ServerSerializer
 
 # Create your views here.
 
-class ServerListViewSet(viewsets.ViewSet):
 
+class ServerListViewSet(viewsets.ViewSet):
     queryset = Server.objects.all()
 
-    def list(self,request):
+    def list(self, request):
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
         server_id = request.query_params.get("server_id")
-
+        with_num_members = request.query_params.get("with_num_members") == "true"
 
         if by_user or server_id and not request.user.is_authenticated:
             raise AuthenticationFailed()
 
         if category:
-            self.queryset = self.queryset.filter(category__name = category)
+            self.queryset = self.queryset.filter(category__name=category)
 
         if by_user:
             user_id = request.user.id
-            self.queryset = self.queryset.filter(member = user_id)
+            self.queryset = self.queryset.filter(member=user_id)
+
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
 
         if server_id:
             try:
-                self.queryset = self.queryset.filter(id = server_id)
+                self.queryset = self.queryset.filter(id=server_id)
                 if not self.queryset.exists():
-                    raise ValidationError(detail = f"Server with Id {server_id} does not exists")
+                    raise ValidationError(detail=f"Server with Id {server_id} does not exists")
             except ValueError:
                 raise ValidationError(detail=f"Server Value Error")
-
 
         if qty:
             self.queryset = self.queryset[: int(qty)]
 
-    
-        serializer = ServerSerializer(self.queryset, many = True)
+        serializer = ServerSerializer(self.queryset, many=True,context = {"num_members":with_num_members})
         return Response(serializer.data)
